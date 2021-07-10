173998c8249a18443904912fb77cc6f2b8384108
Allan Wilson <akwilson76@googlemail.com>
2021-07-10 18:45:02 +0100

    Improve the ThrottledConsumer
    Break checking of the log in to a separate thread safe function. Remove
    old items from the log that are no longer relevant.

--- lib/src/main/java/com/wilsonak/nairn/throttler/ThrottledConsumer.java
+++ lib/src/main/java/com/wilsonak/nairn/throttler/ThrottledConsumer.java
@@ -4,7 +4,6 @@ import com.wilsonak.nairn.RingBuffer;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalUnit;
 import java.util.Queue;
 import java.util.Set;
 import java.util.TreeSet;
@@ -12,62 +11,66 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 /**
- * Throttle calls to a {@code Consumer}.
+ * Throttle calls to a {@code Consumer} at the specified rate.
  *
  * @param <T> type of the parameter to be consumed
  */
 public class ThrottledConsumer<T> implements Consumer<T> {
+    private final Object locker = new Object();
     private final ScheduledExecutorService scheduler = new ScheduledThreadPoolExecutor(1);
-    private final Set<LocalDateTime> log = new TreeSet<>();
     private final Queue<T> buffer;
     private final Consumer<T> consumer;
-    private final int rate;
+    private final int maxInPeriod;
     private final ChronoUnit timeUnit;
+    private Set<LocalDateTime> log;
 
     /**
      * Initialises a new instance of the {@code ThrottledConsumer} class
      *
-     * @param consumer consumer call to throttle
-     * @param rate     number of calls per time unit
-     * @param timeUnit time units to throttle by
-     * @param backlog  number of items to queue while throttling
+     * @param consumer    consumer call to throttle
+     * @param maxInPeriod number of calls per time unit
+     * @param timeUnit    time units to throttle by
+     * @param backlog     number of items to queue while throttling
      */
-    public ThrottledConsumer(Consumer<T> consumer, int rate, ChronoUnit timeUnit, int backlog) {
+    public ThrottledConsumer(Consumer<T> consumer, int maxInPeriod, ChronoUnit timeUnit, int backlog) {
         this.buffer = new RingBuffer<>(backlog);
+        this.log = new TreeSet<>();
         this.consumer = consumer;
-        this.rate = rate;
+        this.maxInPeriod = maxInPeriod;
         this.timeUnit = timeUnit;
     }
 
-    private boolean checkTimes(T t) {
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime periodAgo = now.minus(1, timeUnit);
+    /**
+     * Counts the number of calls made within the last time period. If the number is within the
+     * maximum rate then the we are within the throttle limits.
+     *
+     * @return true if the call rate is within the desired limit
+     */
+    private boolean checkThrottle() {
+        synchronized (locker) {
+            LocalDateTime now = LocalDateTime.now();
+            LocalDateTime periodAgo = now.minus(1, timeUnit);
 
-        int cnt = 0;
-        for (LocalDateTime ldt : log) {
-            if (ldt.isAfter(periodAgo)) {
-                cnt++;
-            }
+            Set<LocalDateTime> newLog = log.stream()
+                                           .filter(ldt -> ldt.isAfter(periodAgo))
+                                           .collect(Collectors.toCollection(TreeSet::new));
+            newLog.add(now);
+            log = newLog;
+
+            return newLog.size() <= maxInPeriod;
         }
-
-        log.add(now);
-
-        if (cnt < rate) {
-            return true;
-        } else {
-            buffer.offer(t);
-            scheduler.schedule(() -> accept(buffer.poll()), 1, TimeUnit.of(timeUnit));
-        }
-
-        return false;
     }
 
     @Override
     public void accept(T t) {
-        if (checkTimes(t)) {
+        if (checkThrottle()) {
             consumer.accept(t);
+        } else {
+            buffer.offer(t);
+            scheduler.schedule(() -> accept(buffer.poll()), 1, TimeUnit.of(timeUnit));
         }
     }
 }
--- lib/src/test/java/com/wilsonak/nairn/throttler/ThrottledConsumerTests.java
+++ lib/src/test/java/com/wilsonak/nairn/throttler/ThrottledConsumerTests.java
@@ -12,6 +12,10 @@ import static org.junit.Assert.assertEquals;
  * Unit tests for the {@link ThrottledConsumer} class.
  */
 public class ThrottledConsumerTests {
+
+    /**
+     * Throttle to five per second. Should return five times from six calls.
+     */
     @Test
     public void testFivePerSecond() {
         AtomicInteger result = new AtomicInteger(0);
@@ -22,6 +26,9 @@ public class ThrottledConsumerTests {
         assertEquals("Wrong number of calls", 5, result.get());
     }
 
+    /**
+     * Throttle to five per second. Call five, wait, then call again. All six should return.
+     */
     @Test
     public void testFivePerSecondWaitBefore() throws Exception {
         AtomicInteger result = new AtomicInteger(0);
@@ -34,14 +41,31 @@ public class ThrottledConsumerTests {
         assertEquals("Wrong number of calls", 6, result.get());
     }
 
+    /**
+     * Throttle to five per second. Call six. All six should return after a second's wait.
+     */
     @Test
     public void testFivePerSecondWaitAfter() throws Exception {
         AtomicInteger result = new AtomicInteger(0);
         ThrottledConsumer<String> cut = new ThrottledConsumer<>(s -> result.incrementAndGet(), 5, ChronoUnit.SECONDS, 10);
 
         IntStream.range(0, 6).forEach(i -> cut.accept("AAA"));
-        Thread.sleep(1500);
+        Thread.sleep(1100);
 
         assertEquals("Wrong number of calls", 6, result.get());
+    }
+
+    /**
+     * Throttle to 5 per second. Should return 15 of 20 calls in 3 seconds.
+     */
+    @Test
+    public void testLongRunning() throws Exception {
+        AtomicInteger result = new AtomicInteger(0);
+        ThrottledConsumer<String> cut = new ThrottledConsumer<>(s -> result.incrementAndGet(), 5, ChronoUnit.SECONDS, 10);
+
+        IntStream.range(0, 20).forEach(i -> cut.accept("AAA"));
+        Thread.sleep(3000);
+
+        assertEquals("Wrong number of calls", 15, result.get());
     }
 }
